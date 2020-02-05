22568e2eaca6fb2b37824e5d2a748799d0a134ae
PavelSlastenov <p.slastenov@mail.ru>
2020-02-05 10:13:55 +0300

    Добавил переменные

---
+++ js/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+//  Количество объявлений
+var NUMBER_OF_ADS = 8;
+
+// Тип жилья
+var TYPE_OF_HOUSING = ['palace','flat','house','bungalo'];
+
+// Время заселения/Выселения
+var CHECK_TIME = ['12:00','13:00','14:00'];
+
+//Комфортные опции
+var OPTIONS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
+
+// Цены
+var MIN_PRICE = 5000;
+var MAX_PRICE = 100000;
+
+// Количество комнат
+var MIN_ROOMS = 1;
+var MAX_ROOMS = 5;
+
+// Количество гостей
+var MIN_GUESTS = 1;
+var MAX_GUESTS = 10;
+
+// Валюта
+var CURRENCY = '₽';
+
+// Фотографии неджвижимости
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+// Размеры метки
+var PIN_WIDTH = 100;
+var PIN_HEIGHT = 100;
+
+//Координаты метки
+var LOCATION_X_MIN = 0;
+var LOCATION_Y_MIN = 130;
+var LOCATION_Y_MAX = 630;
+
