1165b4d5507fa2ef36ba970279b16f7c9da18fb8
PavelSlastenov <p.slastenov@mail.ru>
2020-02-05 18:32:36 +0300

    Переписал переменные (объеденил min и max значения в одну

--- js/main.js
+++ js/main.js
@@ -12,10 +12,18 @@ var PRICE = {
   MIN: 200,
   MAX: 10000
 };
-var MIN_ROOMS = 1;
-var MAX_ROOMS = 5;
-var MIN_GUESTS = 1;
-var MAX_GUESTS = 10;
+var ROOMS = {
+  MIN: 1,
+  MAX: 5
+};
+var GUESTS = {
+  MIN: 1,
+  MAX: 10
+};
+var ADDRESS = {
+  MAX1: 300,
+  MAX2: 1111
+};
 var CURRENCY = '₽';
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var TITLE = [
@@ -39,58 +47,26 @@ var DESCRIPTION = [
   'Предложение для тех кто хочет сэкономить, раскладушка, обогреватель, холодная вода (горячая у соседей), холодильник за окном'
 ];
 
-// Размеры метки
+//  Размер карты
+var MAP_WIDTH = 1200;
+
+//  Размеры метки
 var PIN_WIDTH = 100;
 var PIN_HEIGHT = 100;
 
 //  Координаты метки
-var LOCATION_X_MIN = 0;
-var LOCATION_Y_MIN = 130;
-var LOCATION_Y_MAX = 630;
+var Y = {
+  MIN: 130,
+  MAX: 630
+};
 
 //  Метка объявления
-var similarMapPin = document.querySelector('#pin').content.querySelector('.map__pin');
-var pinWidth = document.querySelector('.map__pin').offsetWidth;
-var pinHeight = document.querySelector('.map__pin').offsetHeight;
-
-//  Переменная задающая размер блока где будет находится метка на карте
-var maxWidth = document.querySelector('.map__pins').offsetWidth;
+var mapElement = document.querySelector('.map');
+var ListElement = document.querySelector('.map__pins');
+var PinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var fragment = document.createDocumentFragment();
 
 //  Функция выбора случайного числа
 var getRandomNumber = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 };
-
-//  Функция выбора элемента из массива (можно применить к любому масиву)
-var getRandomElement = function (arr) {
-  if (!arr) {
-    return null;
-  } else {
-    var random = Math.floor(Math.random() * arr.length);
-    return arr[random];
-  }
-};
-
-//  Функция создающая объекты в массиве
-var getAdList = function (number) {
-  var adList = [];
-  for (var i = 0; i < number; i++) {
-    var X = getRandomElement(LOCATION_Y_MIN, maxWidth);
-    var Y = getRandomElement(LOCATION_Y_MIN, LOCATION_Y_MAX);
-    adList.push({
-      author: {
-//  Случайная адресная строка для "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png,
-//  где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются.
-        avatar: 'img/avatars/user' + '0' + getRandomNumber(1, 8) + '.png'
-      }
-      location: {
-        x: X,
-        y: Y
-        // "x": случайное число, координата x метки на карте, ограничено размерами блока, в котором перетаскивается метка.
-        // "y": случайное число, координата y метки на карте от 130 до 630 (в переменных: LOCATION_Y_MIN = 130 LOCATION_Y_MAX = 630)
-      },
-      offer: {
-        title: TITLE[getRandomElement(TITLE.length)],
-        address: coordinateX + ' , ' + coordinateY,
-        price: getRandomElement(MIN_PRICE, MAX_PRICE),
-
