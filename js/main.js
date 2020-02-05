9d0393dd42938a76e8ac2fc093a584138a0647c6
PavelSlastenov <p.slastenov@mail.ru>
2020-02-05 10:37:45 +0300

    Переделал структуру переменных

--- js/main.js
+++ js/main.js
@@ -1,40 +1,26 @@
 'use strict';
 
-//  Количество объявлений
 var NUMBER_OF_ADS = 8;
 
-// Тип жилья
+//  Всё содержимое объявления
 var TYPE_OF_HOUSING = ['palace','flat','house','bungalo'];
-
-// Время заселения/Выселения
-var CHECK_TIME = ['12:00','13:00','14:00'];
-
-//Комфортные опции
+var CHECKIN = ['12:00','13:00','14:00'];
+var CHECKOUT = ['12:00','13:00','14:00'];
 var OPTIONS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
-
-// Цены
 var MIN_PRICE = 5000;
 var MAX_PRICE = 100000;
-
-// Количество комнат
 var MIN_ROOMS = 1;
 var MAX_ROOMS = 5;
-
-// Количество гостей
 var MIN_GUESTS = 1;
 var MAX_GUESTS = 10;
-
-// Валюта
 var CURRENCY = '₽';
-
-// Фотографии неджвижимости
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 // Размеры метки
 var PIN_WIDTH = 100;
 var PIN_HEIGHT = 100;
 
-//Координаты метки
+//  Координаты метки
 var LOCATION_X_MIN = 0;
 var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
