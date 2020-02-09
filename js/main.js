6440ca625a8366052b8c8b366de95513f5fa3d9a
PavelSlastenov <p.slastenov@mail.ru>
2020-02-09 09:40:31 +0300

    Исправил ошибки

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var OPTIONS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
 
-var Price = {
+var Prices = {
   MIN: 200,
   MAX: 10000
 };
@@ -50,12 +50,12 @@ var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
 //  Координаты метки
-var CoordinateX = {
+var CoordinatesX = {
   MIN: 0,
   MAX: 1200
 };
 
-var CoordinateY = {
+var CoordinatesY = {
   MIN: 130,
   MAX: 630
 };
@@ -73,50 +73,50 @@ var getRandomNumber = function (min, max) {
 
 //  Функция создающая объекты в массиве
 var getList = function (number) {
-  var List = [];
+  var appartments = [];
   for (var i = 0; i < number; i++) {
-    List.push({
+    appartments.push({
       author: {
         //  Случайная адресная строка для "avatar", число от 1-8 с ведущим 0 (адрес изображения не повторяетсся)
         avatar: 'img/avatars/user' + '0' + (i + 1) + '.png'
       },
       offer: {
-        title: TITLE[getRandomNumber(TITLE.length - 1)],
+        title: TITLE[getRandomNumber(0, TITLE.length - 1)],
         address: location.x + ', ' + location.y,
-        price: getRandomNumber(Price.MIN, Price.MAX),
-        type: TYPE_OF_HOUSING[getRandomNumber(TYPE_OF_HOUSING.length - 1)],
+        price: getRandomNumber(Prices.MIN, Prices.MAX),
+        type: TYPE_OF_HOUSING[getRandomNumber(0, TYPE_OF_HOUSING.length - 1)],
         rooms: getRandomNumber(Rooms.MIN, Rooms.MAX),
         guests: getRandomNumber(Guests.MIN, Guests.MAX),
-        checkin: CHECKIN[getRandomNumber(CHECKIN.length - 1)],
-        checkout: CHECKOUT[getRandomNumber(CHECKOUT.length - 1)],
-        features: OPTIONS[getRandomNumber(OPTIONS.length - 1)],
-        description: DESCRIPTION[getRandomNumber(DESCRIPTION.length - 1)],
-        photos: PHOTOS[getRandomNumber(PHOTOS.length - 1)],
+        checkin: CHECKIN[getRandomNumber(0, CHECKIN.length - 1)],
+        checkout: CHECKOUT[getRandomNumber(0, CHECKOUT.length - 1)],
+        features: OPTIONS[getRandomNumber(0, OPTIONS.length - 1)],
+        description: DESCRIPTION[getRandomNumber(0, DESCRIPTION.length - 1)],
+        photos: PHOTOS[getRandomNumber(0, PHOTOS.length - 1)],
 
         //  "x": случайное число, координата x метки на карте, ограничено размерами блока, в котором перетаскивается метка.
         //  "y": случайное число, координата y метки на карте от 130 до 630 (в переменной: Y)
         location: {
-          x: getRandomNumber(CoordinateX),
-          y: getRandomNumber(CoordinateY)
+          x: getRandomNumber(CoordinatesX.MIN, CoordinatesX.MAX),
+          y: getRandomNumber(CoordinatesY.MIN, CoordinatesY.MAX)
         }
       }
     }
     );
 
   }
-  return List;
+  return appartments;
 };
 
-var pins = getList();
+var pins = getList(NUMBER_OF_ADS);
 
-var renderPin = function () {
+var renderPin = function (pinData) {
   var pinElement = pinTemplate.cloneNode(true);
   var pinImgElement = pinElement.querySelector('img');
 
-  pinElement.style.left = location.x - PIN_WIDTH / 2 + 'px';
-  pinElement.style.top = location.y - PIN_HEIGHT + 'px';
-  pinImgElement.src = avatar;
-  pinImgElement.alt = title;
+  pinElement.style.left = pinData.offer.location.x - PIN_WIDTH / 2 + 'px';
+  pinElement.style.top = pinData.offer.location.y - PIN_HEIGHT + 'px';
+  pinImgElement.src = pinData.author.avatar;
+  pinImgElement.alt = pinData.offer.title;
 
   return pinElement;
 };
